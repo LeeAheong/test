7af016dee5d9c798173e359a333ffc7e4a5e4afa
SNAKE\admin\dev1 <snake.ohgiraffers@gmail.com>
2024-01-30 08:20:39 +0900

    출력 추가

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+
+        System.out.println("HelloWorld");
 
 
     }
